e7f10c729f7b93efd3844c39c4478c09d027a955
Gilberto <gsardas@gmail.com>
2019-01-02 10:43:51 -0200

    Crido atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -26,6 +27,7 @@ if __name__ == '__main__':
     del luciano.filhos
     print(luciano.__dict__)
     print(renzo.__dict__)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
 
 
 
