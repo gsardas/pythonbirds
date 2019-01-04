04f2fc0c41d8fe28462ea4db79ff025c56af6027
Gilberto <gsardas@gmail.com>
2019-01-04 09:09:19 -0200

    Implemantada direcao

--- oo/carro.py
+++ oo/carro.py
@@ -1,3 +1,4 @@
+
 """
 Voce deve criar uma classe carro que vai possuir
 dois atributos compostos por outras classes:
@@ -41,6 +42,7 @@ os seguintes atributos:
     >>> motor.frear()
     >>> motor.velocidade
     0
+
     >>> # Testando direcao
     >>> direcao = Direcao()
     >>> direcao.valor
@@ -69,6 +71,8 @@ os seguintes atributos:
     >>> direcao.girar_a_esquerda()
     >>> direcao.valor
     'Norte'
+
+    >>> # Testando carro
     >>> carro = Carro(direcao, motor)
     >>> carro.calcular_velocidade()
     0
@@ -81,14 +85,49 @@ os seguintes atributos:
     >>> carro.frear()
     >>> carro.calcular_velocidade()
     0
-
-
-
-
-
-
-
-
-
+    >>> carro.calcular_direcao()
+    'Norte'
+    >>> carro.girar_a_direita()
+    >>> carro.calcular_direcao()
+    'Leste'
+    >>> carro.girar_a_esquerda()
+    >>> carro.calcular_direcao()
+    'Norte'
+    >>> carro.girar_a_esquerda()
+    >>> carro.calcular_direcao()
+    'Oeste'
 
 """
+
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
+
+class Direcao:
+    ### quando existirem muitos IFs, uma opcao e criar um dicionario
+    rotacao_a_direita_dct = {NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE}
+    rotacao_a_esquerda_dct = {NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE}
+
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
+
+
+
+
+class Motor:
+    def __init__(self):
+        self.velocidade = 0
+
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        self.velocidade -= 2
+        self.velocidade = max(0, self.velocidade)
